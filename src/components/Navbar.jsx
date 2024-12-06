ea07bcb04f1a1d11bc895c4ce9abfcba8f5f1308
Yead <yead19@gmail.com>
2024-12-06 22:15:53 +0600

    theme added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import userImg from '../assets/user.png'
 
@@ -8,8 +8,8 @@ import toast from 'react-hot-toast';
 import { AuthContext } from '../AuthProvider/AuthProvider';
 
 
-
 const Navbar = () => {
+
     const navigate = useNavigate()
     const { user, logOut } = useContext(AuthContext)
     const handleSignOut = () => {
@@ -30,9 +30,17 @@ const Navbar = () => {
         <li><NavLink to='/myEquipments' >My Product</NavLink></li>
 
     </div >
+    const [theme, setTheme] = useState("light");
+
+    const toggleTheme = () => {
+        const newTheme = theme === "light" ? "dark" : "light";
+        setTheme(newTheme);
+        document.documentElement.setAttribute("data-theme", newTheme);
+    };
+
 
     return (
-        <div className="navbar bg-base-200 shadow-md fixed top-0 z-50 lg:px-20">
+        <div className="navbar bg-base-200 shadow-md fixed top-0 z-50 lg:px-20 ">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -65,7 +73,28 @@ const Navbar = () => {
 
                 </ul>
             </div>
-            <div className="navbar-end gap-3">
+            <div className="navbar-end gap-3 ">
+                <button className="btn shadow-none" onClick={toggleTheme}>
+                    {theme === "light" ?
+                        <svg
+                            className="swap-on h-10 w-10 fill-current"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24">
+                            <path
+                                d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                        </svg>
+
+                        :
+                        <svg
+                            className="swap-off h-10 w-10 fill-current"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24">
+                            <path
+                                d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                        </svg>
+
+                    }
+                </button>
                 <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className={`w-10 rounded-full hover:${user?.displayName}`} >
@@ -81,7 +110,7 @@ const Navbar = () => {
 
 
                         </div>
-                       
+
                     </div>
                     <ul
                         tabIndex={0}
@@ -112,7 +141,7 @@ const Navbar = () => {
                             :
                             <Link to='/login' className="btn bg-base-100 "><PiSignIn className='text-lg' /> Login</Link>
                     }
-                            {/* <Link to='/login' className="btn bg-base-100 "><PiSignIn className='text-lg' /> Login</Link> */}
+                    {/* <Link to='/login' className="btn bg-base-100 "><PiSignIn className='text-lg' /> Login</Link> */}
 
 
                 </div>
